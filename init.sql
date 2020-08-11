58121222796980c3d8a8e1951aebdab88890c24c
傅双波 <fusb@tuya.com>
2020-08-11 23:35:16 +0800

    更新init sql

--- init.sql
+++ init.sql
@@ -26,6 +26,18 @@ CREATE TABLE "dashboard" (
   PRIMARY KEY ("id")
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE "datasource" (
+  "id" bigint(13) unsigned NOT NULL AUTO_INCREMENT,
+  "gmt_create" bigint(13) unsigned NOT NULL,
+  "gmt_modified" bigint(13) unsigned NOT NULL,
+  "status" tinyint(1) unsigned NOT NULL,
+  "type" int(4) unsigned NOT NULL,
+  "description" varchar(128) NOT NULL DEFAULT '',
+  "name" varchar(64) NOT NULL,
+  "config" varchar(1024) NOT NULL DEFAULT '',
+  PRIMARY KEY ("id")
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE "node" (
   "id" bigint(13) unsigned NOT NULL AUTO_INCREMENT,
   "name" varchar(32) NOT NULL DEFAULT '',
@@ -36,7 +48,7 @@ CREATE TABLE "node" (
   "gmt_modified" bigint(13) unsigned NOT NULL,
   "status" tinyint(1) unsigned NOT NULL,
   PRIMARY KEY ("id")
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='节点表';
 
 CREATE TABLE "work_sheet" (
   "id" bigint(13) unsigned NOT NULL AUTO_INCREMENT,
@@ -47,8 +59,25 @@ CREATE TABLE "work_sheet" (
   "table_name" varchar(64) NOT NULL DEFAULT '',
   "title" varchar(64) NOT NULL DEFAULT '',
   "description" varchar(128) NOT NULL DEFAULT '',
-  "field_infos" varchar(2048) NOT NULL DEFAULT '' COMMENT '字段元数据信息',
   "source_config" varchar(2024) NOT NULL DEFAULT '' COMMENT '数据源配置',
   "last_sync_time" bigint(13) unsigned NOT NULL COMMENT '最近数据同步时间',
+  "datasource_id" bigint(13) unsigned NOT NULL,
   PRIMARY KEY ("id")
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE "work_sheet_field" (
+  "id" bigint(13) unsigned NOT NULL AUTO_INCREMENT,
+  "name" varchar(64) NOT NULL DEFAULT '',
+  "title" varchar(64) NOT NULL DEFAULT '',
+  "column_type" int(4) unsigned NOT NULL,
+  "field_type" int(4) unsigned NOT NULL,
+  "remarks" varchar(256) NOT NULL DEFAULT '',
+  "seq_no" int(1) unsigned NOT NULL,
+  "link_url" varchar(512) NOT NULL DEFAULT '',
+  "config" varchar(1024) NOT NULL DEFAULT '',
+  "work_sheet_id" bigint(13) unsigned NOT NULL,
+  "gmt_create" bigint(13) unsigned NOT NULL,
+  "gmt_modified" bigint(13) unsigned NOT NULL,
+  "status" int(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY ("id")
+) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8mb4;
